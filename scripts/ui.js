beed0fa4466869929627298f0d92eb68ebd24c67
Bo <Bo@lottotech.com>
2015-07-29 06:24:19 +0300

    added SVG for start menu

--- scripts/ui.js
+++ scripts/ui.js
@@ -564,3 +564,46 @@ $(document ).ready(function() {
     });
 });
 
+var current_frame, total_frames, path, length, handle, logo;
+
+logo = document.getElementById('logo').cloneNode(true);
+
+var init = function () {
+    current_frame = 0;
+    total_frames = 120;
+    path = new Array();
+    length = new Array();
+    for (var i = 0; i < 6; i++) {
+        path[i] = document.getElementById('i' + i);
+        l = path[i].getTotalLength();
+        length[i] = l;
+        path[i].style.strokeDasharray = l + ' ' + l;
+        path[i].style.strokeDashoffset = l;
+    }
+    handle = 0;
+}
+
+
+var draw = function () {
+    var progress = current_frame / total_frames;
+    if (progress > 1) {
+        window.cancelAnimationFrame(handle);
+    } else {
+        current_frame++;
+        for (var j = 0; j < path.length; j++) {
+            path[j].style.strokeDashoffset = Math.floor(length[j] * (1 - progress));
+        }
+        handle = window.requestAnimationFrame(draw);
+    }
+};
+
+init();
+draw();
+
+var rerun = function () {
+    var old = document.getElementsById('logo')[0];
+    old.parentNode.removeChild(old);
+    document.getElementsByTagName('body')[0].appendChild(logo);
+    init();
+    draw();
+};
